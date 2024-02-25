9bd853330d7a3904a16cd1c9922dd438f1f54100
anton2920 <anton2920@gmail.com>
2024-02-25 16:56:28 +0300

    Fixed mmap syscall

--- src/alef/lib/sys/asm_unix.s
+++ src/alef/lib/sys/asm_unix.s
@@ -65,3 +65,42 @@ ok2:
 	MOVL	DX, 4(AX)
 	MOVL	CX, (AX)
 	RET
+
+
+/*
+(int, int, int)
+syscall9(int, int, int, int, int, int, int, int, int, int);
+*/
+TEXT	syscall9(SB), $44
+	/* NOTE(anton2920): saving return tuple address. */
+	MOVL	AX, 40(SP)
+
+	/* NOTE(anton2920): using MOVSL as mem-mem MOVs aren't supported otherwise. */
+	MOVL	trap+0(FP), AX
+	LEAL	a1+4(FP), SI
+	LEAL	4(SP), DI
+	CLD
+	MOVSL
+	MOVSL
+	MOVSL
+	MOVSL
+	MOVSL
+	MOVSL
+	MOVSL
+	MOVSL
+	MOVSL
+	INT	$0x80
+	JAE	ok3
+	MOVL	AX, CX
+	MOVL	40(SP), AX
+	MOVL	CX, 8(AX)
+	MOVL	$-1, 4(AX)
+	MOVL	$-1, (AX)
+	RET
+ok3:
+	MOVL	AX, CX
+	MOVL	40(SP), AX
+	MOVL	$0, 8(AX)
+	MOVL	DX, 4(AX)
+	MOVL	CX, (AX)
+	RET
